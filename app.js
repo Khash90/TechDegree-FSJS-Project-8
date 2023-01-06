7cd779b3d1ea93c831b2a8eab7f128a82ef3c856
Khash <khashayar.m.003@gmail.com>
2023-01-07 00:58:46 +0330

    push

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// database connection
 (async () => {
   try {
     await Sequelize.sync();
