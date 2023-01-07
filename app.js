556920fd56108c0f1ee1276a35394521ff46f879
Khash <khashayar.m.003@gmail.com>
2023-01-07 19:15:58 +0330

    setting error image

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const Sequelize = require('./models/index.js').sequelize;
 var indexRouter = require('./routes/index');
 
 
+
 var app = express();
 
 // view engine setup
@@ -31,19 +32,24 @@ app.use('/', indexRouter);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use( (req, res, next) => {
+  const err = new Error();
+  err.statusCode = 404;
+  err.message = "Sorry ! the page you are looking for doesn't exist";
+  res.render('page-not-found', {title: "Page Not Found", err});
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  err.status = err.status ? err.status : 500;
+  err.message = err.message ? err.message : "Sorry,,,The was an issue with the server!";
+
+  console.log(err.status, err.message)
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(err.status);
+  res.render('error', {title: "Page Not Found" , err});
 });
 
 module.exports = app;
