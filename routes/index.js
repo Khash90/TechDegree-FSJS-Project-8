37baab9a3797be8ec1ac6fa72dc755eccbde5c81
Khash <khashayar.m.003@gmail.com>
2023-01-07 23:20:56 +0330

    routes done

--- routes/index.js
+++ routes/index.js
@@ -25,4 +25,80 @@ router.get('/books', asyncHandler(async(req, res) => {
   res.render('index', { books });
 }));
 
+
+/* Create new book */
+router.get("/new", (req,res) => {
+  res.render("new-book", {book: {}, title: "New Book "})
+});
+
+/*Posts a new book to the database*/
+router.post("/new", asyncHandler(async(req,res) => {
+  let book;
+  try {
+    book = await Book.create(req.body);
+    res.redirect("/");
+  } catch (error) {
+    if (error.name === " SequelizeValidationError") {
+      book = await Book.build(req.body);
+      res.render("new-book", {book, errors: error.errors , title: "New Book"})
+    } else {
+      throw error
+    }
+    
+  }
+}))
+
+/*Shows book detail form */
+router.get("/:id", asyncHandler(async (req,res) => {
+  const book = await Book.findByPk(req.params.id);
+  if(book){
+    res.render("update-book", {book, title: book.title })
+  } else {
+    const err = new Error();
+    err.status = 404;
+    err.message = "Book Id Doesn't Exist"
+    res.render("page-not-found", {title: "Page Not Found", err });
+  }
+}));
+
+/*Updates book info in the database*/
+router.post("/:id", asyncHandler(async(req,res) => {
+  let book;
+  try {
+     book = await Book.findByPk(req.params.id);
+    if (book) {
+      await book.update(req.body);
+      res.redirect("/")
+    } else {
+      const err = new Error();
+      err.status = 404;
+      err.message = "Book Id Doesn't Exist"
+      res.render("page-not-found", {title: "Page-Not-Found", err });
+    }
+  } catch (error) {
+    if (error.name === "SequelizeValidationErrors") {
+      book = await Book.build(req.body);
+      book.id = req.params.id;
+      res.render("update-book", {book, errors: error.errors, title: "New Book "})
+    } else {
+      throw error;
+    }
+  }
+}));
+
+/* Delete a book */
+
+router.post("/:id/delete", asyncHandler(async(req,res) => {
+  const book = await Book.findByPk(req.params.id)
+  if(book) {
+    await book.destroy();
+    res.redirect("/");
+  }else{
+    const err = new Error();
+    err.status = 404;
+    err.message = "Book Id Doesn't Exist";
+    res.render("page-not-found", {title: "Page-Not-Found", err });
+  }
+}))
+
 module.exports = router;
